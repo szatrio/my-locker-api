e27454152ec767e6f8e9887bbd9fffcb9f14e3d1
Satrio Utomo <satrioutomo@gramedia.id>
2024-03-27 21:59:20 +0700

    initial commit

---
+++ src/main/java/com/example/mylocker/MyLockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mylocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLockerApplication.class, args);
+	}
+
+}
